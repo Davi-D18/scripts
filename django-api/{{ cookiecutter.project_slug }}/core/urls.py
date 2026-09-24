9c80079b8c93aec5604b01936abcd8193b19fa25
Davi-D18 <luizdavi.code@gmail.com>
2026-09-24 16:43:54 -0300

    feat(django-api): adicionar favicon e rota /favicon.ico

--- django-api/{{ cookiecutter.project_slug }}/core/urls.py
+++ django-api/{{ cookiecutter.project_slug }}/core/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.templatetags.static import static
 from django.urls import path, include
 from django.conf import settings
 {%- if cookiecutter.use_documentation == "yes" %}
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 {% endif %}
 
+
+def favicon(request):
+    """Redireciona /favicon.ico para o arquivo estático (resolvido na requisição)."""
+    return redirect(static('favicon.ico'), permanent=True)
+
+
 urlpatterns = [
    path('admin/', admin.site.urls),
-   # path('api/v1/', include('apps.nome-app.urls')),
+   path('favicon.ico', favicon),
+   # path('api/v1/', include('apps.nome_app.urls')),
    {%- if cookiecutter.use_authentication == "yes" %}
    path('api/v1/auth/', include('apps.authentication.urls')),
    {%- endif %}
